cecc6025858e6a4db739c929ca6d1711c35efb62
technodumb <alnaskabeer@gmail.com>
2024-07-03 16:12:24 +0530

    All the http commands

--- app.ts
+++ app.ts
@@ -1,14 +1,17 @@
 import express from "express";
-import { employeeRouter } from "./employeeRoutes";
+import employeeRouter from "./employeeRoutes";
+import loggerMiddleware from "./loggerMiddleware";
+import bodyParser from "body-parser";
 
 const server = express();
+server.use(loggerMiddleware);
+server.use(bodyParser.json());
+server.use(employeeRouter);
 
 server.get("/", (req, res) => {
     console.log(req.url);
     res.status(200).send("Hello World");
 });
-
-server.use(employeeRouter);
 
 server.listen(3000, () => {
     console.log("server listening to 3000");
