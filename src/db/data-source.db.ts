edc6b187b057d34d165ce951ea853af67c06328a
technodumb <alnaskabeer@gmail.com>
2024-07-05 17:34:38 +0530

    refactor: move sensitive data to .env

--- src/db/data-source.db.ts
+++ src/db/data-source.db.ts
@@ -1,14 +1,18 @@
 import "reflect-metadata";
 import { DataSource } from "typeorm";
 import { SnakeNamingStrategy } from "typeorm-naming-strategies";
+import dotenv from "dotenv";
+
+dotenv.config();
+console.log(process.env.PASSWORD);
 
 const dataSource = new DataSource({
     type: "postgres",
-    host: "localhost",
-    port: 5435,
-    database: "training",
-    username: "alnas1",
-    password: "dockerpass",
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT),
+    database: process.env.DB_NAME,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
     extra: { max: 5, min: 2 },
     synchronize: false,
     logging: true,
