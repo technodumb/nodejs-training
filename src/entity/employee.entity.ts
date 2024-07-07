f9bda5117669645e18aa690a646b0c8bb3df73ff
technodumb <alnaskabeer@gmail.com>
2024-07-07 19:12:10 +0530

    add: create department entity and add relation with employee entity

--- src/entity/employee.entity.ts
+++ src/entity/employee.entity.ts
@@ -1,7 +1,8 @@
-import { Column, Entity, OneToOne } from "typeorm";
+import { Column, Entity, ManyToOne, OneToOne } from "typeorm";
 import AbstractEntity from "./abstract-entity";
 import Address from "./address.entity";
 import { Role } from "../utils/role.enum";
+import Department from "./department.entity";
 
 @Entity()
 class Employee extends AbstractEntity {
@@ -14,11 +15,11 @@ class Employee extends AbstractEntity {
     @Column()
     age: Number;
 
-    @OneToOne(() => Address, (address) => address.employee, {
-        cascade: true,
-        onDelete: "CASCADE",
-    })
+    @OneToOne(() => Address, (address) => address.employee,)
     address: Address;
+
+    @ManyToOne(()=>Department, (department) => department.employees)
+    department: Department;
 
     @Column({ nullable: true })
     password: string;
