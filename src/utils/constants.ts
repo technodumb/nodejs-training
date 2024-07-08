7b1a63ba1e3f3d95307ff63055aac50f1974aac5
technodumb <alnaskabeer@gmail.com>
2024-07-08 07:43:29 +0530

    fix: add jwt secrets to .env

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,2 +1,6 @@
-export const JWT_SECRET = "ABCDE123";
-export const JWT_VALIDITY = "24H";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+export const JWT_SECRET = process.env.JWT_SECRET;
+export const JWT_VALIDITY = process.env.JWT_VALIDITY;
